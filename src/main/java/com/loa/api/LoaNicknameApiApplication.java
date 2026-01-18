9ddc3efa7d425993dba4801b6516f07eed6c1881
weeyoungseok <dndrldzk@gmail.com>
2026-01-18 22:23:15 +0900

    feat: Initialize project

---
+++ src/main/java/com/loa/api/LoaNicknameApiApplication.java
@@ -0,0 +1,13 @@
+package com.loa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaNicknameApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoaNicknameApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loa/api/LoaNicknameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaNicknameApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
