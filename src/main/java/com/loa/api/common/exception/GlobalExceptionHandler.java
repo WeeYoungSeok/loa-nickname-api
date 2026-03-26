b3903f92af02f9b4c819059175ded8ec87c186a4
weeyoungseok <dndrldzk@gmail.com>
2026-03-27 01:40:59 +0900

    chore: 불필요한 import문 수정 및 삭제

--- src/main/java/com/loa/api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/loa/api/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.loa.api.common.exception;
 
-import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
